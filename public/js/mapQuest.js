d8fa09b6f83c815290b486da3859c1ae488a1005
Anand Suresh <aks037@ucsd.edu>
2021-02-25 10:30:36 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'K5NCOpkV3Fs5QFTNqP6AUe9ZBeAY9ZWE';
+
+	var map = L.mapquest.map('map', {
+		center: [32.87922310278212, -117.23601474603319],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.87922310278212, -117.23601474603319]).addTo(map);
 }
+
